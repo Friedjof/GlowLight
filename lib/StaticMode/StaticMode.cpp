d4d04850e24f1bacbe96bd16acd9a6ff212ba7cb
friedjof@noweck.info <friedjof@noweck.info>
2024-12-22 18:25:07 +0100

    fix: colors StaticMode

--- lib/StaticMode/StaticMode.cpp
+++ lib/StaticMode/StaticMode.cpp
@@ -11,37 +11,37 @@ StaticMode::StaticMode(LightService* lightService, DistanceService* distanceServ
 
 void StaticMode::setup() {
   this->addOption("Warm soft yellow", [this]() {
-    this->lightService->fill(CRGB(255, 230, 160));
+    this->lightService->fill(CRGB(255, 240, 180));
   }, false);
   this->addOption("Warm lavender", [this]() {
-    this->lightService->fill(CRGB(220, 160, 245));
+    this->lightService->fill(CRGB(230, 170, 255));
   }, false);
   this->addOption("Extra warm white", [this]() {
-    this->lightService->fill(CRGB(255, 200, 150));
+    this->lightService->fill(CRGB(255, 220, 170));
   }, false);
   this->addOption("Warm soft green", [this]() {
-    this->lightService->fill(CRGB(140, 200, 140));
+    this->lightService->fill(CRGB(160, 220, 160));
   }, false);
   this->addOption("Warmer soft blue", [this]() {
-    this->lightService->fill(CRGB(170, 190, 220));
+    this->lightService->fill(CRGB(190, 210, 240));
   }, false);
   this->addOption("Warm coral", [this]() {
-    this->lightService->fill(CRGB(255, 135, 85));
+    this->lightService->fill(CRGB(255, 155, 105));
   }, false);
   this->addOption("Warmer pink", [this]() {
-    this->lightService->fill(CRGB(255, 160, 180));
+    this->lightService->fill(CRGB(255, 180, 200));
   }, false);
   this->addOption("Gold", [this]() {
-    this->lightService->fill(CRGB(255, 200, 50));
+    this->lightService->fill(CRGB(255, 220, 70));
   }, false);
   this->addOption("Red", [this]() {
-    this->lightService->fill(CRGB(220, 50, 50));
+    this->lightService->fill(CRGB(240, 70, 70));
   }, false);
   this->addOption("Lime", [this]() {
-    this->lightService->fill(CRGB(100, 255, 100));
+    this->lightService->fill(CRGB(120, 255, 120));
   }, false);
   this->addOption("Blue", [this]() {
-    this->lightService->fill(CRGB(80, 120, 255));
+    this->lightService->fill(CRGB(100, 140, 255));
   }, false);
 }
 
